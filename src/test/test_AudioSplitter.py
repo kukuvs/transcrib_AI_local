dd3866a07835e6be9e9b1a1ef862c0f96a3f426a
kukuvs <danil.bakhmetev05@gmail.com>
2024-08-25 03:52:06 +0400

    WhisperTranscriber test

--- src/test/test_AudioSplitter.py
+++ src/test/test_AudioSplitter.py
@@ -1,8 +1,5 @@
 import pytest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
-from AudioSplitter import AudioSplitter
+from ..AudioSplitter import AudioSplitter
 from unittest.mock import patch
 from pydub import AudioSegment
 
