838120fa03ce2ad21e9a6acbf35e80b80fbde90e
kukuvs <danil.bakhmetev05@gmail.com>
2024-09-10 17:04:58 +0400

    логирование остального кода

--- src/FileManager.py
+++ src/FileManager.py
@@ -1,8 +1,18 @@
+import logging
+
+# Настройка логирования
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class FileManager:
     @staticmethod
     def write_text_to_file(text, filename):
-        if not text or None:  # Проверка на пустой текст или None
+        if not text:  # Проверка на пустой текст или None
+            logging.warning(f"Attempt to write empty or None text to file '{filename}'")
             return
-        with open(filename, "a", encoding="utf-8") as file:
-            file.write(text)
+
+        try:
+            with open(filename, "a", encoding="utf-8") as file:
+                file.write(text)
+                logging.info(f"Successfully wrote text to file '{filename}'")
+        except Exception as e:
+            logging.error(f"Failed to write text to file '{filename}': {e}")
